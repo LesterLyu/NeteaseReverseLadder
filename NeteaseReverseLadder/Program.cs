edb8c3a9fe043dc15956392e123672bea8c045c0
lyudishu <lester.lyu@mail.utoronto.ca>
2017-07-31 04:06:54 -0400

    Update

--- NeteaseReverseLadder/Program.cs
+++ NeteaseReverseLadder/Program.cs
@@ -2,14 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Runtime.InteropServices;
 
 namespace NeteaseReverseLadder
 {
     class Program
     {
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
+
         static void Main(string[] args)
         {
-            start:
+            var handle = GetConsoleWindow();
+            if(args.Length == 1 && args[0].Contains("-h"))
+                // Hide
+                ShowWindow(handle, SW_HIDE);
+            else
+                // Show
+                ShowWindow(handle, SW_SHOW);
+
+        start:
             var ps = new ProxySelector();
             while (true)
             {
